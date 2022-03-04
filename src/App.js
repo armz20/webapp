3bad183294686d3eb406d6339565939427ebb682
Armz <amiah015@gmail.com>
2022-03-04 10:50:24 -0500

    search page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+    <div className="main">
+    <div>
+    <div className = 'div1'>
+ 
+    <header>
+         <a class = "cta" href="#"><button></button></a>
+       <nav>
+        <div className='logo'>LOGO</div>
+     
+      </nav>
+       <ul class='nav__links'>
+         <li><a href="default.asp">Profile</a></li>
+         <li><a href="default.asp">Sign-In</a></li>
+      </ul>
+  
+    </header>
     </div>
+
+      <input className="Search"
+        type="search"
+        placeholder="Seach" width="500" height="500"
+      />
+    <div className="loading">Showing results for...</div>
+    <div className="content">
+
+    <div className="box1">
+    </div>
+    <div className="box2">
+    </div>
+    <div className="box3">
+    </div>
+    
+   </div>
+   </div>
+   </div>
   );
 }
 
